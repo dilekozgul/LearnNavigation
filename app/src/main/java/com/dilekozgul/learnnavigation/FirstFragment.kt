8e1b8a0e1a6c94abb6b1d6668c692c1b2851ac03
Dilek Özgül <dilekozgul9@gmail.com>
2021-11-24 17:53:56 +0300

    Learn Navigation

---
+++ app/src/main/java/com/dilekozgul/learnnavigation/FirstFragment.kt
@@ -0,0 +1,42 @@
+package com.dilekozgul.learnnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+
+
+class FirstFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("Argument")
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+
+}
